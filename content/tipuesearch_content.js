6408007335c602622bf747ed6f47aadb2624580d
s40723224 <40723224@gm.nfu.edu.tw>
2021-03-12 19:32:35 +0800

    4time

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': '博麗神社的賽錢箱', 'text': '\n \n', 'tags': '', 'url': '博麗神社的賽錢箱.html'}, {'title': 'SmileDAH的裸照', 'text': '請輸入密碼，以解開加密網頁。   解密 \n \n   \n \n Design by Cotpear _End', 'tags': '', 'url': 'SmileDAH的裸照.html'}]};
+var tipuesearch = {"pages": [{'title': '博麗神社的賽錢箱', 'text': '\n \n', 'tags': '', 'url': '博麗神社的賽錢箱.html'}, {'title': 'SmileDAH的裸照', 'text': '請輸入密碼，以解開加密網頁。   解密 \n \n \n \n Design by Cotpear _End', 'tags': '', 'url': 'SmileDAH的裸照.html'}]};
